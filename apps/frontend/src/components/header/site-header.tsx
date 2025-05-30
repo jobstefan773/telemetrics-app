ecfd3c74595391b2b36a63744d8ae2b55ad05d61
Arj Tabudlong <108502620+aaaaaahrj@users.noreply.github.com>
2025-05-31 01:04:38 +0800

    feat: Dark mode toggle on header

--- apps/frontend/src/components/header/site-header.tsx
+++ apps/frontend/src/components/header/site-header.tsx
@@ -17,6 +17,8 @@ import { SidebarRoute, sidebarRoute } from "@/routes/routes";
 
 import { usePathname } from "next/navigation";
 
+import ModeToggle from "../theme/mode-toggle";
+
 export function SiteHeader({ routes }: { routes: SidebarRoute[] }) {
   const pathname = usePathname();
   const crumbs = routes.filter(
@@ -25,33 +27,42 @@ export function SiteHeader({ routes }: { routes: SidebarRoute[] }) {
 
   return (
     <header className='group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 flex h-12 shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear'>
-      <div className='flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6'>
-        <SidebarTrigger className='-ml-1' />
-        <Separator
-          orientation='vertical'
-          className='mx-2 data-[orientation=vertical]:h-4'
-        />
-        <Breadcrumb>
-          <BreadcrumbList>
-            <BreadcrumbItem>
-              <BreadcrumbLink href='/dashboard/home'>Dashboard</BreadcrumbLink>
-            </BreadcrumbItem>
-            {crumbs.map((route, idx) => (
-              <React.Fragment key={route.url}>
-                <BreadcrumbSeparator />
+      <div className='flex w-full items-center px-4 lg:gap-2 lg:px-6'>
+        <div className='flex w-full items-center justify-between'>
+          <div className='flex items-center gap-1'>
+            <SidebarTrigger className='-ml-1' />
+            <Separator
+              orientation='vertical'
+              className='mx-2 data-[orientation=vertical]:h-4'
+            />
+            <Breadcrumb>
+              <BreadcrumbList>
                 <BreadcrumbItem>
-                  {idx === crumbs.length - 1 ? (
-                    <BreadcrumbPage>{route.title}</BreadcrumbPage>
-                  ) : (
-                    <BreadcrumbLink href={route.url}>
-                      {route.title}
-                    </BreadcrumbLink>
-                  )}
+                  <BreadcrumbLink href='/dashboard/home'>
+                    Dashboard
+                  </BreadcrumbLink>
                 </BreadcrumbItem>
-              </React.Fragment>
-            ))}
-          </BreadcrumbList>
-        </Breadcrumb>
+                {crumbs.map((route, idx) => (
+                  <React.Fragment key={route.url}>
+                    <BreadcrumbSeparator />
+                    <BreadcrumbItem>
+                      {idx === crumbs.length - 1 ? (
+                        <BreadcrumbPage>{route.title}</BreadcrumbPage>
+                      ) : (
+                        <BreadcrumbLink href={route.url}>
+                          {route.title}
+                        </BreadcrumbLink>
+                      )}
+                    </BreadcrumbItem>
+                  </React.Fragment>
+                ))}
+              </BreadcrumbList>
+            </Breadcrumb>
+          </div>
+          <div>
+            <ModeToggle />
+          </div>
+        </div>
       </div>
     </header>
   );
