30531123302de212dd24ce48db13fcdde3bca986
Arj Tabudlong <108502620+aaaaaahrj@users.noreply.github.com>
2025-06-02 21:21:19 +0800

    feat: login page

--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 import { ThemeProvider } from "@/components/theme/theme-provider";
-import { ModeToggle } from "@/components/theme/mode-toggle";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
